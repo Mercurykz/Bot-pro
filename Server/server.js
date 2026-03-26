79aa2b07928c0430afe14a9759fa41a6306b0ab9
Mercurykz <141973598+Mercurykz@users.noreply.github.com>
2026-03-26 13:46:00 -0300

    Initialize Express server with Discord authentication

---
+++ Server/server.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const passport = require('./routes/auth');
+
+const app = express();
+
+app.use(session({
+  secret: 'segredo',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// rota login
+app.get('/login', passport.authenticate('discord'));
+
+// callback
+app.get('/callback',
+  passport.authenticate('discord', {
+    failureRedirect: '/'
+  }),
+  (req, res) => {
+    res.redirect('/dashboard');
+  }
+);
+
+// dashboard
+app.get('/dashboard', (req, res) => {
+  if (!req.user) return res.redirect('/');
+
+  res.send(`
+    <h1>Logado como ${req.user.username}</h1>
+    <img src="https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png"/>
+    <pre>${JSON.stringify(req.user.guilds, null, 2)}</pre>
+  `);
+});
+
+// home
+app.get('/', (req, res) => {
+  res.send('<a href="/login">Login com Discord</a>');
+});
+
+app.listen(3000, () => {
+  console.log('🌐 Painel com login rodando');
+});
