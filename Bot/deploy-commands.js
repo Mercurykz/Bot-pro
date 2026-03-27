bc0b1c48f785c2253967a8f05265f6bf18e28f72
Mercurykz <141973598+Mercurykz@users.noreply.github.com>
2026-03-27 13:18:18 -0300

    Create deploy-commands.js

---
+++ Bot/deploy-commands.js
@@ -0,0 +1,25 @@
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+require('dotenv').config();
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('presenca')
+    .setDescription('Registrar presença')
+].map(cmd => cmd.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Registrando comandos...');
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log('Comandos registrados!');
+  } catch (err) {
+    console.error(err);
+  }
+})();
