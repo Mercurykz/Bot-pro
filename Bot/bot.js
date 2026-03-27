1e52212b5638759afe7bb61e5eb740769ee1faaf
Mercurykz <141973598+Mercurykz@users.noreply.github.com>
2026-03-27 13:55:28 -0300

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -28,5 +28,31 @@ client.on(Events.InteractionCreate, async interaction => {
     await interaction.reply('✅ Presença registrada!');
   }
 });
+const hoje = new Date().toISOString().split('T')[0];
+
+db.get(
+  `SELECT * FROM presencas 
+   WHERE user_id = ? AND guild_id = ? AND data LIKE ?`,
+  [interaction.user.id, interaction.guild.id, hoje + '%'],
+  (err, row) => {
+
+    if (row) {
+      return interaction.reply('⚠️ Você já marcou presença hoje!');
+    }
+
+    db.run(
+      `INSERT INTO presencas (user_id, username, guild_id, data)
+       VALUES (?, ?, ?, ?)`,
+      [
+        interaction.user.id,
+        interaction.user.username,
+        interaction.guild.id,
+        new Date().toISOString()
+      ]
+    );
+
+    interaction.reply('✅ Presença registrada!');
+  }
+);
 
 client.login(process.env.TOKEN);
